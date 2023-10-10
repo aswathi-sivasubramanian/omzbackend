e62dbf1fb2f75ca609bbd9f44b25209eb44210fb
Navanee-7 <nkhellboy7007@gmail.com>
2023-10-10 09:10:07 +0000

    Added Home Page view

---
+++ dashboard/dashboard/views.py
@@ -0,0 +1,5 @@
+from django.http import HttpResponse
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("Hello Openmynz, please see API endpoints at /api/v1/(app_name)")
